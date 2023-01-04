a78ba1f232a61c1697dde595fd4493bc361db9b1
Marien Mupenda <marien.mupenda@gmail.com>
2023-01-04 14:52:25 +0200

    remove views

--- src/LaravelMediaServiceProvider.php
+++ src/LaravelMediaServiceProvider.php
@@ -3,8 +3,6 @@
 namespace SmirlTech\LaravelMedia;
 
 use Illuminate\Support\ServiceProvider;
-use Livewire\Livewire;
-use SmirlTech\LaravelMedia\Livewire\MediaAttachment;
 
 class LaravelMediaServiceProvider extends ServiceProvider
 {
@@ -12,8 +10,8 @@ class LaravelMediaServiceProvider extends ServiceProvider
     {
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
         $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'laravel-media');
+        /*$this->loadViewsFrom(__DIR__ . '/../resources/views', 'laravel-media');
 
-        Livewire::component('media-attachment', MediaAttachment::class);
+        Livewire::component('media-attachment', MediaAttachment::class);*/
     }
 }
