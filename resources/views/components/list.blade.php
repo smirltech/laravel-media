dcea00946a21aabb99e7fcd76eed9b234bda55be
Marien Mupenda <marien.mupenda@gmail.com>
2026-07-05 06:44:07 +0200

    fix media on array

--- resources/views/components/list.blade.php
+++ resources/views/components/list.blade.php
@@ -1,21 +1,37 @@
 @props(['media','delete' => false])
 <ol class="list-group mt-3">
-    @foreach($media as $m)
+    @if(is_array($media))
+        @foreach($media as $m)
+            <li class="list-group-item">
+                <i class="fa fa-{{$m->icon}}"></i>
+                <a class="" title="Voir"
+                   href="{{route('media.show', $m)}}"
+                   target="_blank">{{$m->filename}}</a>
+                @if($delete)
+                    |
+                    <button type="button" class="btn btn-sm btn-outline-danger">
+                        <i wire:click="deleteMedia('{{$m->id}}')"
+                           class="fa fa-minus"></i>
+                    </button>
+                @endif
+
+            </li>
+        @endforeach
+    @else
         <li class="list-group-item">
-            <i class="fa fa-{{$m->icon}}"></i>
+            <i class="fa fa-{{$media->icon}}"></i>
             <a class="" title="Voir"
-               href="{{route('media.show', $m)}}"
-               target="_blank">{{$m->filename}}</a>
+               href="{{route('media.show', $media)}}"
+               target="_blank">{{$media->filename}}</a>
             @if($delete)
                 |
                 <button type="button" class="btn btn-sm btn-outline-danger">
-                    <i wire:click="deleteMedia('{{$m->id}}')"
+                    <i wire:click="deleteMedia('{{$media->id}}')"
                        class="fa fa-minus"></i>
                 </button>
             @endif
-
         </li>
-    @endforeach
+    @endif
 </ol>
 
 
